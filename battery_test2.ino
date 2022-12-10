8ed735b370285219ae8f6e9a8f6f75082abcfb00
Petr Slouka <Sloukapetr@gmail.com>
2022-12-10 17:46:08 +0100

    Update battery_test2.ino

--- battery_test2.ino
+++ battery_test2.ino
@@ -1,16 +1,16 @@
+// --- LIBRARIES INCLUDE ---
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
+// --- DEFAULT SETTINGS ---
+#define ESP_ID          "1"
 
-#define ESP_ID          "1/1"
-
-#define WIFI_SSID       "GumaPetr"
-#define WIFI_PASSWORD   "Gottvaldovi77"
+#define WIFI_SSID       "SSID"
+#define WIFI_PASSWORD   "PASS"
 
 #define MQTT_SERVER     "192.168.10.1"
 
-#define LOOP_TIME       10*60*1000
+#define LOOP_TIME       3000 //3 SECS LOOP TIME
 
 WiFiClient wifi;
 PubSubClient client(wifi);
@@ -33,7 +33,6 @@ unsigned int valvesCount = 8;
 void setup_wifi() {
 
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to WiFi:");
   Serial.println(WIFI_SSID);
@@ -45,8 +44,6 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
   }
-
-  //randomSeed(micros());
 
   Serial.println();
   Serial.print("WiFi connected to");
@@ -63,20 +60,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  /*if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }*/
-
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a client ID
@@ -85,10 +71,6 @@ void reconnect() {
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      //client.publish("outTopic", "hello world");
-      //client.publish("inTopic", "intopic/22");
-      // ... and resubscribe
       client.subscribe("sensor/1-1/temperature");
     } else {
       Serial.print("Error! Message: ");
@@ -101,7 +83,6 @@ void reconnect() {
 }
 
 void setup() {
-  //pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(9600);
   setup_wifi();
   client.setServer(MQTT_SERVER, 1883);
@@ -122,7 +103,6 @@ void loop() {
     String valveName = "valve/";
     valveName += valves[i];
     client.publish(String(valveName).c_str(), String(random(0, 100)).c_str(), true);
-    delay(200); //wait 200
   }
 
   delay(LOOP_TIME);
